dca0c124162037667278f7fdeb131c2dd61b9c55
Kenny Zhou <l.w.r.f.42@icloud.com>
2023-11-24 18:00:23 +0800

    test npy and npz speed

--- notebooks/test_loading_speed.py
+++ notebooks/test_loading_speed.py
@@ -15,7 +15,7 @@ import numpy as np
 
 # %%
 def test_loading(path,times:int,module:str,describe:str):
-    assert module in ["cv2","PIL"]
+    assert module in ["cv2","PIL","npy","npz"]
 
     # if type(path) is Path:
     #     path = str(path)
@@ -25,11 +25,17 @@ def test_loading(path,times:int,module:str,describe:str):
         if module == "cv2":
             img = cv2.imread(str(path))
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
+        elif module == "npy":
+            img = np.load(path)
+            type(img)
+        elif module == "npz":
+            data_from_npz = np.load(path)
+            img = data_from_npz['img']
+            type(img)
         else:
             img = np.asarray(Image.open(path), dtype=np.uint8)
 
-    print(f"{module}读取{describe}图片100次用时：{time.time()-t0:.5f}s")
+    print(f"{module}读取{describe}数据{times}次用时：{time.time()-t0:.5f}s")
 
 # %%
 if __name__ == '__main__':
@@ -50,5 +56,54 @@ if __name__ == '__main__':
 
     test_loading(large_png, 100, "cv2", "large_png")
     test_loading(large_png, 100, "PIL", "large_png")
+
+
+# %%
+def resize_keep_aspect_ratio(image_path, target_width=256):
+    # 读取图像
+    img = cv2.imread(image_path)
+
+    # 获取原始图像的宽度和高度
+    height, width = img.shape[:2]
+
+    # 计算缩放比例
+    ratio = target_width / width
+
+    # 计算新的高度
+    target_height = int(height * ratio)
+
+    # 使用cv2.resize进行缩放
+    resized_img = cv2.resize(img, (target_width, target_height))
+
+    return resized_img
+
+# 示例使用
+resized_image = resize_keep_aspect_ratio(str(middle_jpg))
+print(str(middle_jpg.parent) + "/" + (middle_jpg.stem) + "_resize_256.jpg")
+cv2.imwrite(str(middle_jpg.parent) + "/" + (middle_jpg.stem) + "_resize_256.jpg", resized_image)
+
+# %%
+repetition = 12
+video_counts = 40
+
+test_path = str(middle_jpg.parent) + "/" + (middle_jpg.stem) + "_resize_256.jpg"
+npy_path = str(middle_jpg.parent) + "/" + (middle_jpg.stem) + f"_resize_256_t{repetition}.npy"
+npz_path = str(middle_jpg.parent) + "/" + (middle_jpg.stem) + f"_resize_256_t{repetition}_nums_{video_counts}.npz"
+
+img = cv2.imread(test_path)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+video_array = np.expand_dims(img,0).repeat(repetition,axis=0)
+print(video_array.shape)
+
+assert video_counts>=1
+video_label_pairs = {str(name):video_array for name in range(video_counts-1)}
+np.save(npy_path, video_array)
+np.savez_compressed(npz_path, img=video_array,**video_label_pairs,compress='lzma')
+
+# %%
+test_loading(test_path, 1000*repetition, "cv2", f"middle_resize_256_{repetition}")
+test_loading(npy_path,1000,"npy",f"middle_resize_256_{repetition}")
+test_loading(npz_path,1000,"npz",f"middle_resize_256_{repetition}")
 
 # %%
---
+++ utils/build_np_dataset.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/11/24 15:31
+# @Author  : Kenny Zhou
+# @FileName: build_np_dataset.py
+# @Software: PyCharm
+# @Email    ：l.w.r.f.42@gmail.com
+
+
+import subprocess
+import concurrent.futures
+from pathlib import Path
+import json
+import os
+import signal
+from tqdm import tqdm
+
+from utils.wsl_tools import wsl_to_windows_path
+from utils.GLOBAL_CONSTANTS import VIDEO_EXTENSIONS
+
+import platform
+
+
+def get_system_type():
+	system_type = platform.system()
+	return system_type
+
+
+SYSTEM_TYPE = get_system_type()
+
+STATE_FILE = './resume_state.txt'
+STOP_REQUESTED = False
+DELETE_STATE_FILE = True
+
+
+def signal_handler(signum, frame):
+	global STOP_REQUESTED
+	STOP_REQUESTED = True
+	print("Signal received, stopping...")
+
+
+def save_state(state_data):
+	with open(STATE_FILE, 'w') as f:
+		json.dump(state_data, f)
+
+
+def load_state():
+	if os.path.exists(STATE_FILE):
+		with open(STATE_FILE, 'r') as f:
+			return json.load(f)
+	return []
+
+
+def extract_frames(video_path, output_root, fps: int = 1, platform=False):
+	"""
+	Extract frames from a video using FFmpeg with NVDEC GPU-acceleration.
+	If the video is corrupted or the process fails, it returns False.
+
+	Args:
+		video_path (Path): The path to the video file.
+		output_root (Path): The root path to store the extracted frames.
+
+	Returns:
+		bool: True if frames were extracted successfully, False otherwise.
+	"""
+	global SYSTEM_TYPE
+
+	output_path = output_root / video_path.parent.name / (video_path.stem)
+	output_path.mkdir(parents=True, exist_ok=True)
+
+	# print(output_root)
+	# print(video_path.parent.name)
+	# print(video_path.stem)
+	# print(output_path)
+
+	if SYSTEM_TYPE == "Windows":
+		ffmpeg_path = r"D:\ffmpeg-6.0-full_build\bin\ffmpeg.exe"
+	elif SYSTEM_TYPE == "Linux":
+		ffmpeg_path = '/usr/local/bin/ffmpeg'  # WSL: '/mnt/d/ffmpeg-6.0-full_build/bin/ffmpeg.exe'
+	else:
+		ffmpeg_path = '/opt/homebrew/bin/ffmpeg'
+
+	if platform == "cuda":
+		# FFmpeg command using NVDEC for GPU acceleration
+		hwaccel_parameter = ['-hwaccel', 'nvdec',  # Use CUDA for GPU acceleration
+							 '-hwaccel_device', '0']
+	elif platform == "mps":
+		hwaccel_parameter = ["-hwaccel", "videotoolbox"]
+	else:
+
+		hwaccel_parameter = [""]
+
+	cmd = [
+		ffmpeg_path,
+		*hwaccel_parameter,
+		# '-hwaccel_output_format', 'cuda',  # Set the hardware acceleration output format to CUDA
+		# '-c:v', 'h264_nvenc',  # Use the h264_nvenc decoder
+		'-i', str(video_path),
+		'-vf', f'fps={fps}',
+		str(output_path / f'{video_path.stem}_%05d.jpg')
+	]
+
+	# Execute the FFmpeg command
+	try:
+		subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		print(f"Frames extracted for {video_path}")
+		return True
+	except subprocess.CalledProcessError as e:
+		error_message = e.stderr.decode('utf-8') if e.stderr else 'An unknown error occurred'
+		print(f"Error processing {video_path}: {error_message}")
+		return False
+
+
+def process_all_videos(video_folder, output_root, fps: int = 1, platform="cuda", use_last_state=True):
+	global DELETE_STATE_FILE
+	"""
+	Process all video files in a directory using multithreading.
+	Prints out corrupted videos and skips them.
+
+	Args:
+		video_folder (str): The path to the folder containing video files.
+		output_root (str): The root path to store the extracted frames.
+	"""
+	video_folder = Path(video_folder)
+
+	output_root = Path(output_root)
+
+	if use_last_state:
+		processed_videos = load_state()
+	else:
+		processed_videos = []
+
+	video_files = [video for video in video_folder.glob('**/*') if
+				   video.suffix.lower() in VIDEO_EXTENSIONS and str(video) not in processed_videos]
+
+	corrupted_videos = []
+
+	try:
+
+		# Use ThreadPoolExecutor to process video files in parallel
+		with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
+			futures = {executor.submit(extract_frames, video_file, output_root, fps, platform): video_file for
+					   video_file in
+					   tqdm(video_files, desc="Processing Videos")}
+
+			for future in concurrent.futures.as_completed(futures):
+				if STOP_REQUESTED:
+					break
+				video_file = futures[future]
+				print(f"Processing {str(video_file)}")
+				success = future.result()
+				if success:
+					processed_videos.append(str(video_file))
+					save_state(processed_videos)
+				else:
+					corrupted_videos.append(video_file)
+	except KeyboardInterrupt:
+		print("Interrupted by user. Saving state...")
+		save_state(processed_videos)
+		return
+
+	# Print out corrupted videos
+	if corrupted_videos:
+		DELETE_STATE_FILE = False
+		print("Corrupted videos:")
+		for video in corrupted_videos:
+			print(video)
+
+	# If all videos are processed, remove the state file
+	if not STOP_REQUESTED and DELETE_STATE_FILE:
+		try:
+			print(f"Delete {STATE_FILE}")
+			os.unlink(STATE_FILE)
+		except FileNotFoundError:
+			pass
+
+
+if __name__ == "__main__":
+	"""
+		The tree of files and folders before processing looks like this:
+
+			-out_{fps}fps
+				-A
+					-video_1
+						-video_1_00001.jpg
+						-video_1_00002.jpg
+						...
+					-video_2
+						-video_2_00001.jpg
+						-video_2_00002.jpg
+						...
+				-B
+					-video_3
+						-video_3_00001.jpg
+						-video_3_00002.jpg
+						...
+					-video_4
+						-video_4_00001.jpg
+						-video_4_00002.jpg
+						...
+
+
+		after:
+		
+			-train_XXX_npz
+				-A
+					-video_1.npz
+					-video_2.npz
+					...
+				-B
+					-video_3.npz
+					-video_4.npz
+					...
+
+	"""
+
+	signal.signal(signal.SIGINT, signal_handler)
+	signal.signal(signal.SIGTERM, signal_handler)
+
+	fps = 12
+
+	platform = "cuda"
+	input_videos_path = r'/mnt/g/Kinetics-400/raw-part/compress/train_256'
+	output_frames_path = '/mnt/g/Kinetics-400/raw-part/compress/train_256_frames_12fps'
+
+	# wsl = False
+	# if wsl:
+	input_videos_path = wsl_to_windows_path(input_videos_path)
+	output_frames_path = wsl_to_windows_path(output_frames_path)
+
+	use_last_state = True
+
+	process_all_videos(input_videos_path, output_frames_path, fps, platform, use_last_state)
